2f1c8f8b8b0ce1699587e13b2966b7611cf6f4e3
Administrator <lijunzhangdale@163.com>
2018-08-08 10:01:08 +0800

    集成redis缓存,同时开始redis注解，使用fastjson序列化key,value值

--- src/main/java/com/lijun/AutocodeApplication.java
+++ src/main/java/com/lijun/AutocodeApplication.java
@@ -3,8 +3,10 @@ package com.lijun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching	//开启缓存注解
 @MapperScan("com.lijun.**.dao.inft")
 public class AutocodeApplication {
 
