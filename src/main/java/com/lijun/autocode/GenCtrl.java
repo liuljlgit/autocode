175f595591099ab684146a50fe7631946a6f8e2e
yizhimanong <lijunzhangdale@163.com>
2018-08-18 14:17:59 +0800

    修改dao配置文件

--- src/main/java/com/lijun/autocode/GenCtrl.java
+++ src/main/java/com/lijun/autocode/GenCtrl.java
@@ -27,43 +27,10 @@ public class GenCtrl {
         //接口Dao替换内容
         Map<String, String> replaceMap = GenCommon.createReplaceMap();
         replaceMap.put("${packageName}",GenProperties.controllerPackageOutPath);
-        replaceMap.put("${urlIdList}",String.join("/",createIdUrlList()));
-        replaceMap.put("${PathVariableIdList}",String.join(",",createPathVarList()));
-        replaceMap.put("${IdNotNull}",String.join(" || ",createIdNotNull()));
-        replaceMap.put("${idParamsList}",String.join(",",GenCommon.getEntityIdList()));
-
-        //导入列表请在最后设置
         replaceMap.put("${importList}",GenCommon.changeImportSetToString(importList));
 
         //创建接口文件
         GenCommon.createFile(true,GenProperties.ctrlName,GenProperties.controllerPackageOutPath,GenCommon.replaceTemplateContent("CtrlTemplate",replaceMap));
-    }
-
-    /**
-     * 创建id参数的url列表
-     */
-    public List<String> createIdUrlList(){
-        return GenCommon.getEntityIdList().stream().map(e->{
-            return "{"+e+"}";
-        }).collect(Collectors.toList());
-    }
-
-    /**
-     * 创建id参数变量列表
-     */
-    public List<String> createPathVarList(){
-        return GenCommon.getEntityIdMap().entrySet().stream().map(e->{
-            return "@PathVariable(value=\""+e.getKey()+"\") "+e.getValue()+" "+e.getKey()+"";
-        }).collect(Collectors.toList());
-    }
-
-    /**
-     * 判断主键非空
-     */
-    public List<String> createIdNotNull(){
-        return GenCommon.getEntityIdList().stream().map(e->{
-            return "Objects.isNull("+e+")";
-        }).collect(Collectors.toList());
     }
 
 }
