a0d3065bb13a33b482fecf2a3cd0c17a606c8942
yizhimanong <lijunzhangdale@163.com>
2018-08-18 14:11:36 +0800

    修改dao配置文件

--- src/main/java/com/lijun/autocode/GenDao.java
+++ src/main/java/com/lijun/autocode/GenDao.java
@@ -23,26 +23,13 @@ public class GenDao {
             //接口Dao替换内容
             Map<String, String> inftReplaceMap = GenCommon.createReplaceMap();
             inftReplaceMap.put("${packageName}",GenProperties.daoPackageOutPath.concat(".inft"));
-            inftReplaceMap.put("${@paramIds}",createIdList());
-            //导入列表请在最后设置
             inftReplaceMap.put("${importList}",GenCommon.changeImportSetToString(inftImportList));
+
             //创建接口文件
             GenCommon.createFile(true,GenProperties.inftDaoFileName,GenProperties.daoPackageOutPath.concat(".inft"),GenCommon.replaceTemplateContent("InftDaoTemplate",inftReplaceMap));
 
         }catch(Exception e){
             e.printStackTrace();
         }
-    }
-
-    /**
-     * 创建id列表
-     * @return
-     */
-    public String createIdList(){
-        StringBuffer sb = new StringBuffer("");
-        List<String> priList = GenProperties.tableColumInfoList.stream().filter(e -> e.getTableColumKey().equals("PRI")).map(e -> {
-            return "@Param(\""+e.getEntityPropName()+"\") "+e.getEntityType()+" " +e.getEntityPropName();
-        }).collect(Collectors.toList());
-        return String.join(",",priList);
     }
 }
