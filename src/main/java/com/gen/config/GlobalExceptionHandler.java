77ae1217f56d89ba900b898c7446ded6326c005b
liulijun <liulj@huidiancloud.com>
2018-10-19 16:17:18 +0800

    增加全局异常配置

--- src/main/java/com/gen/config/GlobalExceptionHandler.java
+++ src/main/java/com/gen/config/GlobalExceptionHandler.java
@@ -2,11 +2,14 @@ package com.gen.config;
 
 import com.cloud.common.base.BaseController;
 import com.cloud.common.exception.BusiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * 全局异常处理类
@@ -15,10 +18,16 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler extends BaseController {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     //系统异常
     @ExceptionHandler(BusiException.class)
     @ResponseBody
     public String busiExcepitonHandler(HttpServletRequest request, Exception e) {
+        e.printStackTrace();
+        if(Objects.nonNull(e) && Objects.nonNull(e.getMessage())){
+            return formatResponseParams(EXEC_ERROR,null,e.getMessage());
+        }
         return formatResponseParams(EXEC_ERROR,null);
     }
 
@@ -26,6 +35,10 @@ public class GlobalExceptionHandler extends BaseController {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public String defultExcepitonHandler(HttpServletRequest request, Exception e) {
+        e.printStackTrace();
+        if(Objects.nonNull(e) && Objects.nonNull(e.getMessage())){
+            return formatResponseParams(EXEC_ERROR,null,e.getMessage());
+        }
         return formatResponseParams(EXEC_ERROR,null);
     }
 
