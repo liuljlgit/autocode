7eb4798929a2f559f9b8cbbbedc31c5bcd388fe0
liulijun <liulj@huidiancloud.com>
2019-02-15 17:19:06 +0800

    json转换成dto信息

---
+++ src/main/java/com/gen/autocode/dto/Json2DtoInfo.java
@@ -0,0 +1,71 @@
+package com.gen.autocode.dto;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Json2DtoInfo {
+
+    public void parse(String json, Map<String,Map<String,String>> baseInfo,String clsName) throws Exception {
+        JSONObject obj;
+        if(json.startsWith("{")){
+            obj = JSON.parseObject(json);
+        }else if(json.startsWith("[")){
+            JSONArray array = JSONArray.parseArray(json);
+            obj = array.getJSONObject(0);
+        }else{
+            throw new Exception("字符串不是json字符串");
+        }
+        Set<Map.Entry<String, Object>> entries = obj.entrySet();
+        if(entries.size()>0){
+            Map<String,String> info = new HashMap<>();
+            for (Map.Entry<String, Object> entry : entries) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                if(value instanceof Integer){
+                    info.put(key,"Integer");
+                }else if(value instanceof BigDecimal){
+                    info.put(key,"BigDecimal");
+                }else if(value instanceof String){
+                    info.put(key,"String");
+                }else{
+                    String s = value.toString();
+                    if(s.startsWith("{")){
+                        info.put(key,"Object");
+                        parse(s,baseInfo,key);
+                    }else if(s.startsWith("[{")){
+                        info.put(key,"List");
+                        parse(s,baseInfo,key);
+                    }else if(s.startsWith("[")){
+                        JSONArray sArray = JSONArray.parseArray(s);
+                        Object sObj = sArray.get(0);
+                        if(sObj instanceof Integer){
+                            info.put(key,"List<Integer>");
+                        }else if(sObj instanceof BigDecimal){
+                            info.put(key,"List<BigDecimal>");
+                        }else {
+                            info.put(key,"List<String>");
+                        }
+                    }
+                }
+            }
+            baseInfo.put(clsName,info);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String json ="{\n" +
+                "    \"reqBody\":{\n" +
+                "        \"page\":[1,2,3],\n" +
+                "        \"pageSize\":10\n" +
+                "    }\n" +
+                "}";
+        Json2DtoInfo aa = new Json2DtoInfo();
+        Map<String,Map<String,String>> baseInfo = new HashMap<>();
+        aa.parse(json,baseInfo,"root");
+        System.out.println("aa");
+    }
+}
