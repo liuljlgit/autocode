e77ce30a3aad30fc23afb59b86bde31acc6d48af
liulijun <liulj@huidiancloud.com>
2019-02-20 10:21:52 +0800

    根据json生成dto

--- src/main/java/com/gen/autocode/dto/Json2DtoInfo.java
+++ src/main/java/com/gen/autocode/dto/Json2DtoInfo.java
@@ -4,22 +4,46 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.cloud.common.utils.HumpUtil;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.*;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Json2DtoInfo {
 
-    public void parse(String json, Map<String,Map<String,String>> baseInfo,String clsName) throws Exception {
+    public void parse(String json, Map<String,Map<String,String>> baseInfo,String clsName,int times) throws Exception {
         JSONObject obj;
         if(json.startsWith("{")){
             obj = JSON.parseObject(json);
-        }else if(json.startsWith("[")){
+        }else if(json.startsWith("[{")){
             JSONArray array = JSONArray.parseArray(json);
             obj = array.getJSONObject(0);
+        }else if(times == 1 && json.startsWith("[")){
+            Map<String,String> info = new HashMap<>();
+            JSONArray sArray = JSONArray.parseArray(json);
+            Object sObj = sArray.get(0);
+            if(sObj instanceof Integer){
+                info.put("key","List<Integer>");
+            }else if(sObj instanceof BigDecimal){
+                info.put("key","List<BigDecimal>");
+            }else if(sObj instanceof String){
+                info.put("key","List<String>");
+            }else if(sObj instanceof Boolean){
+                info.put("key","List<Boolean>");
+            }else if(sObj instanceof Date){
+                info.put("key","List<Date>");
+            }
+            baseInfo.put(clsName,info);
+            return;
         }else{
             throw new Exception("字符串不是json字符串");
         }
+        times++;
         Set<Map.Entry<String, Object>> entries = obj.entrySet();
         if(entries.size()>0){
             Map<String,String> info = new HashMap<>();
@@ -40,10 +64,10 @@ public class Json2DtoInfo {
                     String s = value.toString();
                     if(s.startsWith("{")){
                         info.put(HumpUtil.toUpperCaseFirstOne(key),"Object");
-                        parse(s,baseInfo,HumpUtil.toUpperCaseFirstOne(key));
+                        parse(s,baseInfo,HumpUtil.toUpperCaseFirstOne(key),times);
                     }else if(s.startsWith("[{")){
                         info.put(HumpUtil.toUpperCaseFirstOne(key),"List");
-                        parse(s,baseInfo,HumpUtil.toUpperCaseFirstOne(key));
+                        parse(s,baseInfo,HumpUtil.toUpperCaseFirstOne(key),times);
                     }else if(s.startsWith("[")){
                         JSONArray sArray = JSONArray.parseArray(s);
                         Object sObj = sArray.get(0);
@@ -65,6 +89,54 @@ public class Json2DtoInfo {
         }
     }
 
+    /**
+     * 替换模板内容
+     * @param replaceMap
+     * @return
+     */
+    public String replaceTemplateContent(Map<String,String> replaceMap){
+        try{
+            StringBuffer sb = new StringBuffer();
+            ClassPathResource resource = new ClassPathResource("template/dto/DtoTemplate") ;
+            InputStream inputStream = resource.getInputStream();
+            IOUtils.readLines(inputStream).forEach(e->{
+                for(Map.Entry<String, String> data : replaceMap.entrySet()){
+                    e = e.replace(data.getKey(),data.getValue());
+                }
+                sb.append(e);
+                sb.append("\n");
+            });
+            return sb.toString();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 创建文件
+     */
+    public void createFile(String fileName,String packageOutPath,String sb){
+        File directory = new File("");
+        File dirFile;
+        String outputPath;
+        try{
+            dirFile = new File(directory.getAbsolutePath()+ "/src/main/java/"+packageOutPath.replace(".", "/")+"/");
+            if(!dirFile.exists()){
+                dirFile.mkdirs();
+            }
+            outputPath = dirFile.toString()+"/"+fileName + ".java";
+            FileWriter fw = new FileWriter(outputPath);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println(sb.toString());
+            pw.flush();
+            pw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
         String json ="{\n" +
                 "\t\"animals\":{\n" +
@@ -77,11 +149,13 @@ public class Json2DtoInfo {
                 "}";
         Json2DtoInfo json2DtoInfo = new Json2DtoInfo();
         Map<String,Map<String,String>> baseInfo = new HashMap<>();
-        json2DtoInfo.parse(json,baseInfo,"Root");
+        json2DtoInfo.parse(json,baseInfo,"Root",1);
         //遍历生成java文件
         for (Map.Entry<String, Map<String, String>> entry : baseInfo.entrySet()) {
             String clsName = entry.getKey();
             Map<String, String> propertyList = entry.getValue();
+
+            //创建文件
         }
     }
 }
